18a87694965151938aa2d33b137fba394e253896
F1ReKing <284061805@qq.com>
2019-03-21 13:54:18 +0800

    1. 完善

--- app/src/main/java/com/f1reking/flrcviewsample/MainActivity.java
+++ app/src/main/java/com/f1reking/flrcviewsample/MainActivity.java
@@ -2,6 +2,8 @@ package com.f1reking.flrcviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
--- flrcview/src/main/java/com/f1reking/flrcview/FLrcView.java
+++ flrcview/src/main/java/com/f1reking/flrcview/FLrcView.java
@@ -2,11 +2,17 @@ package com.f1reking.flrcview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.os.Looper;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.TextView;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,24 +23,45 @@ import java.util.List;
  */
 public class FLrcView extends View {
 
+    private static final String TAG = FLrcView.class.getSimpleName();
+
+    private int height = 0;
+    private int width = 0;
+    private Paint mPaint;
     private List<LrcEntity> mLrcEntities = new ArrayList<>();
 
     public FLrcView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public FLrcView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public FLrcView(Context context, AttributeSet attrs,
         int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(18);
+        mPaint.setTextAlign(Paint.Align.CENTER);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (width == 0 || height == 0) {
+            width = getMeasuredWidth();
+            height = getMeasuredHeight();
+        }
+        if (mLrcEntities.isEmpty()) {
+            canvas.drawText("暂无歌词", width / 2, height / 2, mPaint);
+            return;
+        }
     }
 
     /**
@@ -46,8 +73,8 @@ public class FLrcView extends View {
             public void run() {
                 new AsyncTask<File, Integer, List<LrcEntity>>() {
                     @Override
-                    protected List<LrcEntity> doInBackground(File... files) {
-                        return LrcEntity.parseLrc(files[0]);
+                    protected List<LrcEntity> doInBackground(File... params) {
+                        return LrcEntity.parseLrc(params[0]);
                     }
 
                     @Override
@@ -68,8 +95,8 @@ public class FLrcView extends View {
             public void run() {
                 new AsyncTask<String, Integer, List<LrcEntity>>() {
                     @Override
-                    protected List<LrcEntity> doInBackground(String... strings) {
-                        return LrcEntity.parseLrc(strings[0]);
+                    protected List<LrcEntity> doInBackground(String... params) {
+                        return LrcEntity.parseLrc(params[0]);
                     }
 
                     @Override
@@ -84,18 +111,43 @@ public class FLrcView extends View {
     /**
      * 加载在线歌词
      */
-    public void loadLrcUrl(final String url) {
-        new AsyncTask<String,Integer,String>(){
+    public void loadLrcUrl(final String lrcUrl) {
+        new AsyncTask<String, Integer, String>() {
 
             @Override
-            protected String doInBackground(String... strings) {
-                return null;
+            protected String doInBackground(String... params) {
+                String lrcText = null;
+                try {
+                    URL url = new URL(params[0]);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setReadTimeout(10000);
+                    if (conn.getResponseCode() == 200) {
+                        InputStream is = conn.getInputStream();
+                        int size = conn.getContentLength();
+                        byte[] buffer = new byte[size];
+                        is.read(buffer);
+                        is.close();
+                        lrcText = new String(buffer);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return lrcText;
             }
 
             @Override
             protected void onPostExecute(String s) {
+                loadLrc(s);
             }
-        }.execute(url);
+        }.execute(lrcUrl);
+    }
+
+    /**
+     * 歌词是否有效
+     */
+    public boolean hasLrc() {
+        return !mLrcEntities.isEmpty();
     }
 
     private void onLoadLrc(List<LrcEntity> entityList) {
@@ -104,11 +156,17 @@ public class FLrcView extends View {
         }
     }
 
+    /**
+     * 刷新时间
+     */
     public void updateTime(final long time) {
         runOnUi(new Runnable() {
             @Override
             public void run() {
-
+                if (!hasLrc()) {
+                    return;
+                }
+                invalidate();
             }
         });
     }
